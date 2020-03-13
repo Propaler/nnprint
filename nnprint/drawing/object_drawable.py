4bf5f9104a4b3c2c226d46d501f0063ee9d5148e
Jeferson <jefersonnpn@gmail.com>
2020-03-12 23:19:24 -0300

    add initial methods for drawable objects

--- nnprint/drawing/object_drawable.py
+++ nnprint/drawing/object_drawable.py
@@ -1,4 +1,18 @@
+from abc import abstractmethod
+
+
 class ObjectDrawable:
     """Defines how to fit any visualization into an output image"""
 
-    pass
+    def __init__(self):
+        super(ObjectDrawable, self).__init__()
+        self._size = (600, 400)  # FIXME set default size
+        self._position = (0, 0)  # FIXME set default position
+
+    def size(self, size):
+        """Sets the size of the drawable object"""
+        self._size = size
+
+    def position(self, position):
+        """Sets the position of the drawable object"""
+        self._position = position
