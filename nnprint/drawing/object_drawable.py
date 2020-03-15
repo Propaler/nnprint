2abea3bdb62da0566e20d9aba09b5a9669f9a733
Jeferson <jefersonnpn@gmail.com>
2020-03-14 23:33:54 -0300

    add object drawable defaults

--- nnprint/drawing/object_drawable.py
+++ nnprint/drawing/object_drawable.py
@@ -4,14 +4,14 @@ from abc import abstractmethod
 class ObjectDrawable:
     """Defines how to fit any visualization into an output image"""
 
-    def __init__(self):
-        super(ObjectDrawable, self).__init__()
-        self._size = (600, 400)  # FIXME set default size
-        self._position = (0, 0)  # FIXME set default position
-
-    def size(self, size):
+    _image_type = "RGB"
+    _shape = (600, 400)  # FIXME set default shape
+    _position = (0, 0)  # FIXME set default top-left position
+    _color = "white"
+    
+    def shape(self, shape):
         """Sets the size of the drawable object"""
-        self._size = size
+        self._shape = shape
 
     def position(self, position):
         """Sets the position of the drawable object"""
